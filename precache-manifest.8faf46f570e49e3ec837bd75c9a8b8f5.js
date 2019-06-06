82e69f78bc7ead1fb5e620ff1eb8065b55f09907
Sio5 <4views.de@gmail.com>
2019-06-06 09:33:09 +0200

    Updates

---
+++ precache-manifest.8faf46f570e49e3ec837bd75c9a8b8f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d515bb223ea0643c0cbd93121f5975b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2111882ecdece2a01a0",
+    "url": "/static/css/main.5cc47da9.chunk.css"
+  },
+  {
+    "revision": "6cccefd5b85811be00b7",
+    "url": "/static/js/2.0b585fdf.chunk.js"
+  },
+  {
+    "revision": "d2111882ecdece2a01a0",
+    "url": "/static/js/main.f13cb162.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
